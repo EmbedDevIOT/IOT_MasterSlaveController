88318ec94bde9930e535870c1983aff1f6bf0f38
EmbedDevIOT <Embeddev.IOT@gmail.com>
2024-04-25 15:31:09 +0300

    v.0.0.3 [25.04.24] StructureUPD

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include "Config.h"
 
-#define DEBUG
+#define DEBUG // Debug control ON
 //======================================================================
 
 //=========================== GLOBAL VARIABLES =========================
 uint8_t sec_cnt = 0;
 char buf[32] = {0}; // buff for send message
 //======================================================================
+
+//================================ OBJECTs =============================
+MicroDS3231 RTC;
+
+OneWire oneWire1(T1);
+OneWire oneWire2(T2);
+DallasTemperature ds18b20_1(&oneWire1);
+DallasTemperature ds18b20_2(&oneWire2);
+
+TaskHandle_t TaskCore_0;
+TaskHandle_t TaskCore_1;
+//=======================================================================
+
+//============================== STRUCTURES =============================
